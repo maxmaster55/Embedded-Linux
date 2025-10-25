1323d3ba213264be73752fc10528b988c25187ee
maxmaster <yousseflkashef000@gmail.com>
2025-10-25 23:11:59 +0300

    added script

---
+++ AdminLinux_Task1/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p ~/myDirectory/secondDirectory
+touch ~/myDirectory/secondDirectory/myNotePaper
+cp ~/myDirectory/secondDirectory/myNotePaper ~/myDirectory/
+mv ~/myDirectory/myNotePaper ~/myDirectory/myOldNotePaper
