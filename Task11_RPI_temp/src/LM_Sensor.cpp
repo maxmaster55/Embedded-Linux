2474317fff8f22b8237d9bf52524067c40fb18e7
maxmaster <yousseflkashef000@gmail.com>
2025-12-06 14:45:27 +0200

    hust some placeholder value

--- Task11_RPI_temp/src/LM_Sensor.cpp
+++ Task11_RPI_temp/src/LM_Sensor.cpp
@@ -4,9 +4,18 @@
 
 LM_Sensor::LM_Sensor(int pin)
 {
-    
+    gpio_pin = pin;
+
+    // later
+
+}
+
+float LM_Sensor::readHumidity(){
+    // later
+    return 3.65; // as a place holder
 }
 
 LM_Sensor::~LM_Sensor()
 {
+    // later
 }
