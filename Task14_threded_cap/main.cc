a119d7bb1e91cc98d8fbc7a301baeb897ae2ba99
maxmaster <yousseflkashef000@gmail.com>
2026-01-26 17:59:43 +0200

    used two threads

--- Task14_threded_cap/main.cc
+++ Task14_threded_cap/main.cc
@@ -4,36 +4,51 @@
 #include <string>
 #include <fstream>
 #include <chrono>
+#include <mutex>
+#include <condition_variable>
 
 using namespace std::chrono_literals;
 
 constexpr std::string_view FILE_PATH = "/sys/class/leds/input6::capslock/brightness";
 
 
+std::mutex mx;
+std::condition_variable cv;
+bool caps_pressed = false;
+
 void checker()
 {
     static bool prev = false;
 
-    std::fstream file(std::string(FILE_PATH), std::ios::in);
-
-    if (!file)
-    {
-        std::cerr << "error, please use sudo\n";
-        return;
-    }
+    std::ifstream file(FILE_PATH.data());
+    if (!file) return;
 
     std::string buffer;
     file >> buffer;
 
     bool curr = (buffer == "1");
 
-    // Detect rising edge (OFF → ON)
-    if (!prev && curr)
-    {
-        std::cout << "CapsLK pressed\n";
+    if (!prev && curr) {
+        std::lock_guard<std::mutex> lock(mx);
+        caps_pressed = true;
     }
+    cv.notify_one();
 
     prev = curr;
+}
+
+
+void listener()
+{
+    std::unique_lock<std::mutex> lock(mx);
+
+    while (true) {
+        cv.wait(lock, [] { return caps_pressed; });
+
+        std::cout << "CapsLK pressed\n";
+
+        caps_pressed = false;
+    }
 }
 
 
@@ -46,12 +61,11 @@ void checker_loop()
     }
 }
 
-
-int main(int argc, char const *argv[])
+int main()
 {
-    std::thread th(checker_loop);
+    std::thread t1(checker_loop);
+    std::thread t2(listener);
 
-
-    th.join();
-    return 0;
+    t1.join();
+    t2.join();
 }
