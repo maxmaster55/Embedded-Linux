0c99a9e2023e05a9b746cf4245476189086ae862
maxmaster <yousseflkashef000@gmail.com>
2026-02-01 09:06:55 +0200

    added service and client code

--- Task15_vsomeip/CapsLK_client.cpp
+++ Task15_vsomeip/CapsLK_client.cpp
@@ -1,7 +1,85 @@
 #include <vsomeip/vsomeip.hpp>
+#include <iostream>
+#include <thread>
 
+
+constexpr static auto APP_NAME = "Hello_Client";
+
+constexpr int SERVICE_ID = 0xAAAA;
+constexpr int INSTANCE_ID = 0x01;
+constexpr int HELLO_METHOD_ID = 0x01;
+constexpr int CAPSLK_METHOD_ID = 0x02;
+
+
+void state_handler(vsomeip::state_type_e _state)
+{
+    if(_state == vsomeip::state_type_e::ST_REGISTERED)
+    {
+        std::cout << "from client register complete \n";
+        std::shared_ptr<vsomeip::application> app = vsomeip::runtime::get()->get_application(APP_NAME);
+        app->request_service(SERVICE_ID, INSTANCE_ID);
+    }
+}
+
+
+void availability_handler(vsomeip::service_t _service, vsomeip::instance_t _inst, bool _flag)
+{
+    std::shared_ptr<vsomeip::application> app = vsomeip::runtime::get()->get_application(APP_NAME);
+
+    std::shared_ptr<vsomeip::message> request = vsomeip::runtime::get()->create_message();
+    request->set_service(_service);
+    request->set_instance(_inst);
+    request->set_method(HELLO_METHOD_ID);
+    request->set_message_type(vsomeip::message_type_e::MT_REQUEST);
+
+    std::shared_ptr<vsomeip::payload> payload =
+        vsomeip::runtime::get()->create_payload();
+    std::vector<vsomeip::byte_t> data = {1, 2, 3};
+    payload->set_data(data);
+    request->set_payload(payload);
+
+    app->send(request);
+}
+
+
+void send_capslock_request(std::shared_ptr<vsomeip::application> app)
+{
+    std::shared_ptr<vsomeip::message> request = vsomeip::runtime::get()->create_message();
+    request->set_service(SERVICE_ID);
+    request->set_instance(INSTANCE_ID);
+    request->set_method(CAPSLK_METHOD_ID);
+    request->set_message_type(vsomeip::message_type_e::MT_REQUEST);
+
+    std::shared_ptr<vsomeip::payload> payload =
+        vsomeip::runtime::get()->create_payload();
+    std::vector<vsomeip::byte_t> data = {7, 8, 9};
+    payload->set_data(data);
+    request->set_payload(payload);
+
+    std::cout << "Sending Caps Lock request\n";
+    app->send(request);
+}
 
 int main(int argc, char **argv)
 {
+    std::shared_ptr<vsomeip::runtime> rtm = vsomeip::runtime::get();
+    std::shared_ptr<vsomeip::application> app = rtm->create_application(APP_NAME);
+   
+    app->init();
 
+    app->register_state_handler(state_handler);
+    app->register_availability_handler(SERVICE_ID, INSTANCE_ID, availability_handler);
+
+    std::thread capslock_thread([app]() {
+        while (true)
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            send_capslock_request(app);
+        }
+    });
+
+    app->start();
+
+    capslock_thread.join();
+    return 0;
 }
--- Task15_vsomeip/CapsLK_service.cpp
+++ Task15_vsomeip/CapsLK_service.cpp
@@ -1,6 +1,118 @@
 #include <vsomeip/vsomeip.hpp>
+#include <iostream>
+#include <csignal>
+
+constexpr static auto APP_NAME = "Hello_Service";
+
+constexpr int SERVICE_ID = 0xAAAA;
+constexpr int INSTANCE_ID = 0x01;
+constexpr int HELLO_METHOD_ID = 0x01;
+constexpr int CAPSLK_METHOD_ID = 0x02;
+
+void signal_handler(int signal)
+{
+    std::shared_ptr<vsomeip::application> app = vsomeip::runtime::get()->get_application(APP_NAME);
+        app->stop_offer_service(SERVICE_ID, INSTANCE_ID);
+        // unregister the state handler
+        app->unregister_state_handler();
+        // unregister the message handler
+        app->unregister_message_handler(
+            SERVICE_ID,
+            INSTANCE_ID,
+            HELLO_METHOD_ID
+        );
+        app->unregister_message_handler(
+            SERVICE_ID,
+            INSTANCE_ID,
+            CAPSLK_METHOD_ID
+        );
+    app->stop();
+    std::cout << "Service stopped due to signal " << signal << std::endl;
+}
+
+
+void state_handler(vsomeip::state_type_e _state)
+{
+    if(_state == vsomeip::state_type_e::ST_REGISTERED)
+    {
+        std::cout << "from service register complete \n";
+    }
+}
+
+void hello_message_handler(const std::shared_ptr<vsomeip::message>& _request)
+{
+    std::cout << "Message received with payload length: "
+              << _request->get_payload()->get_length() << std::endl;
+
+    // Create a response message
+    std::shared_ptr<vsomeip::message> response = vsomeip::runtime::get()->create_message();
+    response->set_service(_request->get_service());
+    response->set_instance(_request->get_instance());
+    response->set_method(HELLO_METHOD_ID);
+    response->set_client(_request->get_client());
+    response->set_message_type(vsomeip::message_type_e::MT_RESPONSE);
+
+    // Create payload for the response
+    std::shared_ptr<vsomeip::payload> payload =
+        vsomeip::runtime::get()->create_payload();
+    std::vector<vsomeip::byte_t> data = {4, 5, 6};
+    payload->set_data(data);
+    response->set_payload(payload);
+
+    // Send the response
+    vsomeip::runtime::get()->get_application(APP_NAME)->send(response);
+}
+
+void capslock_message_handler(const std::shared_ptr<vsomeip::message>& _request)
+{
+    std::cout << "CapsLock Message received with payload length: "
+              << _request->get_payload()->get_length() << std::endl;
+
+    // Create a response message
+    std::shared_ptr<vsomeip::message> response = vsomeip::runtime::get()->create_message();
+    response->set_service(_request->get_service());
+    response->set_instance(_request->get_instance());
+    response->set_method(CAPSLK_METHOD_ID);
+    response->set_client(_request->get_client());
+    response->set_message_type(vsomeip::message_type_e::MT_RESPONSE);
+
+    // Create payload for the response
+    std::shared_ptr<vsomeip::payload> payload =
+        vsomeip::runtime::get()->create_payload();
+    std::vector<vsomeip::byte_t> data = {4, 5, 6};
+    payload->set_data(data);
+    response->set_payload(payload);
+
+    // Send the response
+    vsomeip::runtime::get()->get_application(APP_NAME)->send(response);
+}
+
+
 
 int main(int argc, char **argv)
 {
+    std::shared_ptr<vsomeip::runtime> rtm = vsomeip::runtime::get();
+    std::shared_ptr<vsomeip::application> app = rtm->create_application(APP_NAME);
+   
+    app->init();
 
+    app->register_state_handler(state_handler);
+    app->register_message_handler(
+        SERVICE_ID, 
+        INSTANCE_ID, 
+        HELLO_METHOD_ID,
+        hello_message_handler
+    );
+    app->register_message_handler(
+        SERVICE_ID, 
+        INSTANCE_ID, 
+        CAPSLK_METHOD_ID,
+        capslock_message_handler
+    );
+
+    app->offer_service(SERVICE_ID, INSTANCE_ID);
+
+    app->start();
+
+    return 0;
 }
