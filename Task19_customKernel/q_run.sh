06416f61657be528bea5ffc9244652d8a35908d5
maxmaster <yousseflkashef000@gmail.com>
2026-03-14 16:25:11 +0200

    another hack :(

--- Task19_customKernel/q_run.sh
+++ Task19_customKernel/q_run.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+
+# num is the number printed at the top of qemu
+# use this picocom -b 115200 /dev/pts/{num}
+
 qemu-system-aarch64 \
     -M raspi3b \
     -cpu cortex-a53 \
     -m 1G \
-	-nographic \
     -kernel u-boot.bin \
     -dtb bcm2837-rpi-3-b-plus.dtb \
     -sd vsd.img \
-	-serial none \
-    -serial stdio \
-    -monitor none
+	-nographic \
+	-serial pty \
+	-serial mon:stdio 
+
+
