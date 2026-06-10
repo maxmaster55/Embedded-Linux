73de7fdea2d59c5561703c647de3507630b90b1c
maxmaster <yousseflkashef000@gmail.com>
2026-06-10 23:37:36 +0300

    started working

---
+++ Task24_proc_device_driver/my_module.c
@@ -0,0 +1,24 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+
+static int __init my_module_init(void)
+{
+    printk(KERN_INFO "my_module: loaded\n");
+    return 0; 
+}
+
+
+static void __exit my_module_exit(void)
+{
+    printk(KERN_INFO "my_module: unloaded\n");
+}
+
+
+module_init(my_module_init);
+module_exit(my_module_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Your Name");
+MODULE_DESCRIPTION("A simple kernel module");
